7dff63c3967521e211baef806fdf7b4c32ae1b30
Rahul Agnihotri <rahulagnihotri4444@gmail.com>
2024-12-08 19:22:26 +0530

    08122024

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+var mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI1)
+  .then(() => console.log("Connected to database"))
+  .catch((error) => console.error("Not connected", error));
+
+const app = require("express")();
+const http = require("http").Server(app);
+PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
